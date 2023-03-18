fdd86e5e24178c0838e329fc50b90a85587ef2e1
williamjordan <will.jordan0788@gmail.com>
2023-03-17 17:36:55 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+canvas = Canvas(width=800, height=526)
+canvas_image = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 270, image=canvas_image)
+canvas.grid(column=0, row=0, columnspan=2)
+
+right_image = PhotoImage(file="images/right.png")
+right_button = Button(image=right_image, highlightthickness=0)
+right_button.grid(column=1, row=1)
+
+wrong_image = PhotoImage(file="images/wrong.png")
+wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button.grid(column=0, row=1)
+
+window.mainloop()
+
