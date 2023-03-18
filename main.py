7f5aed816ebde38e38f91fdcc1de34089a951118
williamjordan <will.jordan0788@gmail.com>
2023-03-17 17:53:12 -0700

    'Finished GUI cleaned the image bg and size.'

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 canvas = Canvas(width=800, height=526)
 canvas_image = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 270, image=canvas_image)
+canvas.create_image(400, 263, image=canvas_image)
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
 right_image = PhotoImage(file="images/right.png")
