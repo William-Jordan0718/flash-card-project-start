0040128a8972153e53a013f435eed201912f4d47
williamjordan <will.jordan0788@gmail.com>
2023-03-18 22:42:11 -0700

    'created the function for the buttons to pull through the CSV file'

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from tkinter import *
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+data = pandas.read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient='records')
+def next_card():
+    current_card = random.choice(to_learn)
+    current_card["French"]
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_card["French"])
 
 window = Tk()
 window.title("Flashy")
@@ -10,17 +20,19 @@ canvas = Canvas(width=800, height=526)
 canvas_image = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=canvas_image)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+card_title = canvas.create_text(400, 150, font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, font=("Ariel", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
 right_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_image, highlightthickness=0)
+right_button = Button(image=right_image, highlightthickness=0, command=next_card)
 right_button.grid(column=1, row=1)
 
 wrong_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button = Button(image=wrong_image, highlightthickness=0, command=next_card)
 wrong_button.grid(column=0, row=1)
+
+next_card()
 
 window.mainloop()
 
