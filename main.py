8266c273e853486f019989823b1fd5b4a31ad7a9
williamjordan <will.jordan0788@gmail.com>
2023-03-20 12:45:54 -0700

    fixed the timer on the flip card'

--- main.py
+++ main.py
@@ -9,10 +9,13 @@ to_learn = data.to_dict(orient='records')
 current_card = {}
 
 def next_card():
-    global current_card
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=canvas_image)
+    flip_timer = window.after(3000, func=flip_card)
 
 def flip_card():
     canvas.itemconfig(card_title, text="English", fill="white")
