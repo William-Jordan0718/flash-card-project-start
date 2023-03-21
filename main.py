36498a39104154ff7471d36219ed40f3002378dd
williamjordan <will.jordan0788@gmail.com>
2023-03-21 11:35:48 -0700

    'Completed the flashcard project, now has the functionality to save a list of unknown words.'

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient='records')
 current_card = {}
+to_learn = {}
+
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient='records')
+
 
 def next_card():
     global current_card, flip_timer
@@ -22,11 +29,18 @@ def flip_card():
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
 
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
+
+
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-window.after(3000, func=flip_card)
+flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526)
 canvas_image = PhotoImage(file="images/card_front.png")
@@ -42,7 +56,7 @@ right_button = Button(image=right_image, highlightthickness=0, command=next_card
 right_button.grid(column=1, row=1)
 
 wrong_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0, command=next_card)
+wrong_button = Button(image=wrong_image, highlightthickness=0, command=is_known)
 wrong_button.grid(column=0, row=1)
 
 next_card()
